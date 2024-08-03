3965b75e4c196bcfa63aff7ab10a512b83879bb5
Fahad <fahad.45854@iqra.edu.com>
2024-08-03 20:40:10 +0500

    initial commit as always

---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+})
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist/client',
+//     emptyOutDir: true, 
+//   }
+// });
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   },
+// })
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist/client',
+//     emptyOutDir: true, 
+//   }
+// });
+
+
+
+
+
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
